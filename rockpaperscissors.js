4e6d47f688fd051511acd2960b543fd066ae04e2
Alexander Predhome <apredhome@V9CQ4KH6K2.grubhub.local>
2024-04-24 16:44:46 -0400

    change variables in play round function to be computer choice and human choice.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -23,40 +23,47 @@ getComputerChoice();
 let getHumanChoice = prompt("Rock, Paper, or Scissors?");
 
 if (getHumanChoice.toLowerCase() === "rock"){
-    console.log("you have selected rock.")
+    console.log("You have selected rock.")
 } else if (getHumanChoice.toLowerCase() === "scissors"){
-    console.log("you have selected scissors.")
+    console.log("You have selected scissors.")
 } else if (getHumanChoice.toLowerCase() === "paper"){
-    console.log("you have selected paper.")
+    console.log("You have selected paper.")
 } else {
-    console.log("hmm...that wasn't one of the options.")
+    console.log("Hmm...that wasn't one of the options.")
 }
 
 
 //Create function called playRound
 //Set function to take in human choice and computer choice
-function playRound(getHumanChoice, getComputerChoice){
+function playRound(humanChoice, computerChoice){
     //write code
+
+
+
+    if (humanChoice ==="rock" && computerChoice==="paper"){
+        console.log("You lose! Paper beats rock.")
+    } else if (humanChoice ==="paper" && ccomputerChoice==="scissors"){
+        console.log("You lose! Scissors beats paper.")
+    } else if (humanChoice ==="scissors" && ccomputerChoice==="rock"){
+        console.log("You lose! Rock beats scissors.")
+    } else if (humanChoice ==="paper" && computerChoice==="rock"){ 
+        console.log("You win! Paper beats rock.")
+    } else if (humanChoice ==="scissors" && computerChoice==="paper"){
+        console.log("You win! Scissors beats paper.")
+    } else if (humanChoice ==="rock" && ccomputerChoice==="scissors"){
+        console.log("You win! Rock beats scissors.")
+    } else {
+        console.log("hmm... are you sure you entered that right?")
+    }
+
 }
 
 //Define human selection and computer selection
 let humanSelection = getHumanChoice;
 let computerSelection= getComputerChoice;
 //Define how someone wins
+
 //set a console.log to print who wins
-if (humanSelection ==="rock" && computerSelection==="paper"){
-    console.log("You lose! Paper beats rock.")
-} else if (humanSelection ==="paper" && computerSelection==="scissors"){
-    console.log("You lose! Scissors beats paper.")
-} else if (humanSelection ==="scissors" && computerSelection==="rock"){
-    console.log("You lose! Rock beats scissors.")
-} else if (humanSelection ==="paper" && computerSelection==="rock"){ 
-    console.log("You win! Paper beats rock.")
-} else if (humanSelection ==="scissors" && computerSelection==="paper"){
-    console.log("You win! Scissors beats paper.")
-} else if (humanSelection ==="rock" && computerSelection==="scissors"){
-    console.log("You win! Rock beats scissors.")
-} else {
-    console.log("hmm... are you sure you entered that right?")
-}
+
 //track incremental humanScore or computerScore based on round winner
+
